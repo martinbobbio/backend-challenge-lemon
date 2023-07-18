f3237e75c2373dac22952118a5e213db22f8362c
Martin Bobbio <martinbobbio1@gmail.com>
2023-07-18 02:06:17 -0300

    edit: adjust endpoints

--- src/routes/coins/coins.ts
+++ src/routes/coins/coins.ts
@@ -2,8 +2,6 @@
 import { Request, Response, Router } from "express";
 // Constants
 import { apiCoingecko } from "../../constants";
-// Utils
-import { getTopNRepeatedStrings } from "../../utils";
 // Console colors
 import chalk from "chalk";
 
@@ -18,19 +16,31 @@ const router = Router();
 router.get("/api/coins", async (req: Request, res: Response) => {
   // Getting coins list
   try {
-    const { data: coinsList } = await apiCoingecko.get("/coins/markets", {
-      params: {
-        vs_currency: "usd",
-        order: "market_cap_desc",
-        ids: "bitcoin",
-        per_page: 100,
-        page: 1,
-        sparkline: false,
-      },
-    });
-
-    console.log(chalk.blueBright("/api/coins getting information succesfully"));
-    res.status(200).json(coinsList);
+    const { ids, search } = req.query;
+    const { data: coinsIds } = await apiCoingecko.get("/coins/list");
+    const idsSearched = coinsIds
+      ?.map((ids) => ids.id)
+      ?.filter((id) => id.includes(search))
+      ?.join(",");
+    if (!idsSearched && search) {
+      console.log(chalk.blueBright("/api/coins search not found"));
+      res.status(200).json([]);
+    } else {
+      const { data: coinsList } = await apiCoingecko.get("/coins/markets", {
+        params: {
+          vs_currency: "usd",
+          order: "market_cap_desc",
+          per_page: 100,
+          page: 1,
+          sparkline: false,
+          ids: idsSearched ? idsSearched : ids,
+        },
+      });
+      console.log(
+        chalk.blueBright("/api/coins getting information succesfully")
+      );
+      res.status(200).json(coinsList);
+    }
   } catch (error) {
     console.error(chalk.redBright(`Error searching the item list ${error}`));
     res.status(500).json({ error: "Error searching the item list" });
@@ -45,6 +55,26 @@ router.get("/api/coins", async (req: Request, res: Response) => {
  */
 router.get("/api/coins/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
+  try {
+    const { data: coinsList } = await apiCoingecko.get(`/coins/${id}`, {
+      params: {
+        community_data: false,
+        developer_data: false,
+      },
+    });
+
+    console.log(
+      chalk.blueBright(
+        `/api/coins getting information detail of ${id} succesfully`
+      )
+    );
+    res.status(200).json(coinsList);
+  } catch (error) {
+    console.error(
+      chalk.redBright(`Error searching the item detail ${id} - ${error}`)
+    );
+    res.status(500).json({ error: `Error searching the item detail ${id}` });
+  }
 });
 
 export default router;
--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as getTopNRepeatedStrings } from "./getTopNRepeatedStrings/getTopNRepeatedStrings";
